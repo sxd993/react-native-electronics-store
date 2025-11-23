6d17e42658b41618169b1e01e3ff968d9dc9c403
sxd993 <gamhotik2005@mail.ru>
2025-11-23 23:02:46 +0500

    поиск

--- frontend/src/app/(tabs)/index.tsx
+++ frontend/src/app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
+import { FiltersBar } from "@/features/filters";
 import { ProductList } from "@/widgets/products-list/ProductList";
 import { Text, View } from "react-native";
 
-
 export default function IndexScreen() {
   return (
-    <View className="bg-white flex-1">
-      <Text
-        className="bg-white text-3xl ml-6">
-        Каталог
-      </Text>
-      <ProductList />
+    <View className="bg-white flex-1 p-6">
+      <View>
+        <Text className="text-4xl font-semibold text-gray-900 tracking-tight">Каталог</Text>
+        <Text className="text-gray-500 mt-1">Выбирайте лучшее — от техники до мелочей для дома.</Text>
+      </View>
+
+      <FiltersBar />
+
+      <View className="flex-1">
+        <ProductList />
+      </View>
     </View>
   );
 }
